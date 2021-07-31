99476e8db33d8b0b251126a9807dad6f8dac2a3c
Niels Meima <niels.meima@tno.nl>
2021-07-31 16:22:55 +0200

    feat(nvim): bootstrap installation of paq

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,4 +1,17 @@
 
+-- Bootstrap plugins by checking whether paq-nvim is already installed.
+-- If not, clone and packadd
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/paqs/start/paq-nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', install_path})
+  execute 'packadd paq-nvim'
+end
+
+
 local paq = require("paq")
 paq {
 	"avq/paq-nvim";
