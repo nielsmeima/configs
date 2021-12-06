328ee093bf440135fe3ade54c3a9fda0d09ce4ed
Niels Meima <nielsmeima1@gmail.com>
2021-12-06 17:05:24 +0100

    remove dap

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,20 +1,8 @@
 
 vim.cmd("colorscheme tokyonight")
 vim.cmd("set clipboard^=unnamed,unnamedplus")
-vim.g.clipboard = {
-  name = "win32yank-wsl",
-  copy = {
-    ["+"] = "win32yank.exe -i --crlf",
-    ["*"] = "win32yank.exe -i --crlf"
-  },
-  paste = {
-    ["+"] = "win32yank.exe -o --crlf",
-    ["*"] = "win32yank.exe -o --crlf"
-  },
-  cache_enable = 0,
-}
 vim.cmd("syntax on")
- 
+
 vim.g.mapleader = " "
 vim.g.shellcommandflag = "-ic"
 
